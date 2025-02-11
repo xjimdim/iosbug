c592969c8d2100a77f0c47426e1c55033bdf596c
dkalogirou <xjimdim@gmail.com>
2025-02-11 16:14:44 +0200

    move public output to docs

---
+++ docs/_nuxt/D15bxgs0.js
@@ -0,0 +1,1 @@
+import{_ as s,c as l,o as t,a as o,M as c}from "./gVt6ByXM.js";const a={},f={class:"flex flex-col h-[100vh] h-[100dvh] w-full p-4"},r={class:"flex-1 w-full tw-h-full flex"};function n(e, _){return t(),l("div",f,[o("div",r,[c(e.$slots,"default")])])}const h=s(a,[["render",n]]);export{h as default};
