793dc72cac6ac37af1fdf5cdb1468f9121783487
dkalogirou <xjimdim@gmail.com>
2025-02-11 16:33:14 +0200

    nuxt link for index

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <h2>INDEX PAGE</h2>
     <NuxtLink
       class="text-blue font-semibold cursor-pointer underline mt-4"
-      :href="router.resolve({ name: '__second' }).href"
+      :to="{ name: '__second' }"
     >
       click here to go to second page >
     </NuxtLink>
